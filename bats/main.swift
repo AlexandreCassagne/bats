b7dee7896444530c60dc8427c17fb4f0049e1bd3
Alexandre Cassagne <alexandre.cassagne@gmail.com>
2018-07-11 17:13:14 -0400

    Initial Commit

---
+++ bats/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  bats
+//
+//  Created by Alexandre Cassagne on 11/07/2018.
+//  Copyright © 2018 Sigma Mu. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
